e6d2d2747ee65d6734cdfd009593050efbc2d413
brianjbayer <brianjbayer@gmail.com>
2022-10-31 10:58:47 -0400

    Add user Gravatar to users show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+  # Returns the Gravatar for the given user
+  # See https://www.learnenough.com/course/ruby_on_rails_tutorial_7th_edition/sign_up/showing_users/a_gravatar_image
+  def gravatar_for(user)
+    gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
