c90f52c0e28a0453f57969d22b42fe39f771ecea
brianjbayer <brianjbayer@gmail.com>
2022-11-01 14:42:42 -0400

    Add integration test for invalid signup submission

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test 'invalid signup information' do
+    get signup_path
+    expected_err_count = 4
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: '',
+                                         email: 'user@invalid',
+                                         password: 'foo',
+                                         password_confirmation: 'bar' } }
+    end
+    assert_response :unprocessable_entity
+    assert_template 'users/new'
+    assert_select 'div#error_explanation' do
+      assert_select 'div.alert.alert-danger', /#{expected_err_count} errors/
+      assert_select 'ul>li', expected_err_count
+      # e.g. Name can't be blank
+      assert_select 'ul>li', /name.+blank/i
+      # e.g. Email is invalid
+      assert_select 'ul>li', /email.+invalid/i
+      # Password confirmation doesn't match Password
+      assert_select 'ul>li', /confirmation.+match/i
+      # Password is too short (minimum is 6 characters)
+      assert_select 'ul>li', /Password.+minimum/i
+    end
+    # Both the label and inputs should be wrapped in div.field_with_errors
+    assert_select 'div.field_with_errors', (2 * expected_err_count)
+  end
+end
