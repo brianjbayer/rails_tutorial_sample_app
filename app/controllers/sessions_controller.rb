223362884cea5b8d015b167bbfaa84fc6b010999
brianjbayer <brianjbayer@gmail.com>
2022-11-02 16:31:49 -0400

    Implement invalid login with error message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # TODO: Log the user in and redirect to the user's show page.
+    else
+      # Use flash.now since render is not a request
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+
 end
---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  test 'login with invalid information' do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path params: { session: { email: '', password: '' } }
+    assert_response :unprocessable_entity
+    assert_template 'sessions/new'
+    assert_not_empty flash
+    get root_path
+    assert_empty flash
+  end
+end
