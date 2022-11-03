5c523f83f274428f7dc779f72e66d2d0b78fe14c
brianjbayer <brianjbayer@gmail.com>
2022-11-03 14:48:24 -0400

    Add helper method for getting current userr if logged in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,13 @@
+# frozen_string_literal: true
+
 module SessionsHelper
   # Logs in the given user
   def log_in(user)
     session[:user_id] = user.id
+  end
+
+  # Returns current logged-in user (if any)
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 end
