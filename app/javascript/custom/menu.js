2fea59229fcf4c0b56e09370368a7db487c70e1f
brianjbayer <brianjbayer@gmail.com>
2022-11-03 19:34:30 -0400

    Add styling for mobile

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -4,6 +4,13 @@
 // NOTE: use the turbo page load event "turbo:load"
 //       instead of the JS page load event "DOMContentLoaded"
 document.addEventListener("turbo:load", function() {
+  let hamburger = document.querySelector("#hamburger");
+  hamburger.addEventListener("click", function(event) {
+    event.preventDefault();
+    let menu =document.querySelector("#navbar-menu");
+    menu.classList.toggle("collapse")
+  });
+
   let account = document.querySelector("#account");
   account.addEventListener("click", function(event) {
     event.preventDefault();
