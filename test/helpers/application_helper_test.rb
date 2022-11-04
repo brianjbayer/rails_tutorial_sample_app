f7bb08ab51c57b81f0002c17ede3929947fccdc5
brianjbayer <brianjbayer@gmail.com>
2022-11-04 12:05:50 -0400

    Lint (Rubocop Rails) unique tests

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,7 +1,9 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
-  test "full title helper" do
+  test 'full title helper' do
     base_title = 'Ruby on Rails Tutorial Sample App'
     assert_equal base_title, full_title
     # TODO: Use Faker gem to generate page_title
