718f7760291248fdba9602845e03b6a81362e2db
brianjbayer <brianjbayer@gmail.com>
2022-11-05 15:29:05 -0400

    Add login integration test for login with invalid email

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -7,6 +7,30 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
+  test 'login with valid email/invalid password' do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path params: { session: { email: @user.email,
+                                         password: '' } }
+    assert_response :unprocessable_entity
+    assert_template 'sessions/new'
+    assert_not_empty flash
+    get root_path
+    assert_empty flash
+  end
+
+  test 'login with invalid email/valid password' do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path params: { session: { email: '',
+                                         password: 'password' } }
+    assert_response :unprocessable_entity
+    assert_template 'sessions/new'
+    assert_not_empty flash
+    get root_path
+    assert_empty flash
+  end
+
   test 'login with valid information' do
     post login_path, params: { session: { email: @user.email,
                                           password: 'password' } }
@@ -17,16 +41,5 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', login_path, count: 0
     assert_select 'a[href=?]', logout_path
     assert_select 'a[href=?]', user_path(@user)
-  end
-
-  test 'login with invalid information' do
-    get login_path
-    assert_template 'sessions/new'
-    post login_path params: { session: { email: '', password: '' } }
-    assert_response :unprocessable_entity
-    assert_template 'sessions/new'
-    assert_not_empty flash
-    get root_path
-    assert_empty flash
   end
 end
