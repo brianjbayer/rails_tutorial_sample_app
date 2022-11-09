4ab73734967cf3ea35b58d5bfb6223ecc4c851ee
brianjbayer <brianjbayer@gmail.com>
2022-11-08 19:06:29 -0500

    Fix exception when nil remember digest

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -72,4 +72,8 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = 'a' * 5
     assert_not @user.valid?
   end
+
+  test 'authenticated? returns false for a user with nil remember_digest' do
+    assert_not @user.authenticated?('ignored')
+  end
 end
