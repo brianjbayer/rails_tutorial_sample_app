9d474477db6305b7be2c3255f26f2ac937702b50
brianjbayer <brianjbayer@gmail.com>
2022-11-13 18:06:37 -0500

    Add admin role to User

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -41,8 +41,19 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test 'should redirect update when logged in as wrong user' do
     log_in_as(@other_user)
     patch user_path(@user), params: { user: { name: @user.name,
-                                      email: @user.email } }
+                                              email: @user.email } }
     assert_empty flash
     assert_redirected_to root_url
+  end
+
+  test 'should not allow the admin attribute to be edited via the web' do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+      user: { password: 'password',
+              password_confirmation: 'password',
+              admin: true }
+    }
+    assert_not @other_user.reload.admin?
   end
 end
