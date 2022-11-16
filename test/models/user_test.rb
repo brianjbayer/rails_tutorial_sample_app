41d21811b8d11ee8af25eaf778cb89a279ebdfec
brianjbayer <brianjbayer@gmail.com>
2022-11-16 11:21:34 -0500

    Generalize User authenticated? method

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -74,6 +74,6 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'authenticated? returns false for a user with nil remember_digest' do
-    assert_not @user.authenticated?('ignored')
+    assert_not @user.authenticated?(:remember,'ignored')
   end
 end
