0e649f18cbd4c7884801b5cd449e4a14f951947e
brianjbayer <brianjbayer@gmail.com>
2022-11-18 10:34:47 -0500

    Configure gmail Mailer in development environment

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'user@realdomain.com'
+  default from: Rails.application.config.action_mailer.default_options[:from]
   layout 'mailer'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,14 +36,37 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  # --- CUSTOMIZED MAILER CONFIGURATION ---
   host = ENV.fetch('HOST') { 'localhost' }
   port = ENV.fetch('PORT') { 3000 }
   local_host = "#{host}:#{port}"
   config.action_mailer.default_url_options = { host: local_host, protocol: 'http' }
 
+  if ENV.include?('DISABLE_SENDING_MAIL')
+    # Don't care if the mailer can't send.
+    config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.default_options = { from: 'notsending@email.com' }
+  else
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.default_options = { from: 'no-reply@sampleapp.com' }
+    config.action_mailer.delivery_method = :smtp
+    # SMTP settings for gmail
+    gmail_username = ENV['MAIL_SERVICE_USERNAME']
+    gmail_password = ENV['MAIL_SERVICE_PASSWORD']
+    # config.action_mailer.default_options = { from: "#{gmail_username}@gmail.com" }
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            gmail_username,
+      password:             gmail_password,
+      authentication:       'plain',
+      enable_starttls_auto: true,
+      open_timeout:         5,
+      read_timeout:         5 }
+  end
+  # --- END CUSTOMIZED MAILER CONFIGURATION ---
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
