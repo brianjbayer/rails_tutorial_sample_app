0e1ce7e65971b9aa3aa7552a04e50a817c40c5bd
brianjbayer <brianjbayer@gmail.com>
2022-11-23 15:19:41 -0500

    Refactor User#create_reset_digest to single database transaction

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,6 +53,7 @@ class User < ApplicationRecord
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
     return false if digest.nil?
+
     BCrypt::Password.new(digest).is_password?(token)
   end
 
@@ -72,8 +73,8 @@ class User < ApplicationRecord
 
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest, User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest: User.digest(reset_token),
+                   reset_sent_at: Time.zone.now)
   end
 
   def send_reset_email
