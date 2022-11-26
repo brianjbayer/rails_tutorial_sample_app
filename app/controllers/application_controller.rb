81dc86251b07780643c2c1bd988fcf1ffde0ee54
brianjbayer <brianjbayer@gmail.com>
2022-11-26 18:43:48 -0500

    Enforce logged in for microposts#create and #destroy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@
 
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  private
+
+    # Confirms a logged-in user
+    def logged_in_user
+      unless logged_in?
+        # Store the original requested URl for friendly forwarding
+        store_location
+        flash[:danger] = 'Please log in.'
+        redirect_to login_url, status: :see_other
+      end
+    end
 end
