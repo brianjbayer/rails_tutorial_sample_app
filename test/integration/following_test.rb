0cdfa2f421c35f6b5184d9982ee7706f49bacc07
brianjbayer <brianjbayer@gmail.com>
2022-12-05 16:48:48 -0500

    Add un/follow integration tests and fix

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -5,6 +5,7 @@ require 'test_helper'
 class Following < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @other = users(:archer)
     log_in_as(@user)
   end
 end
@@ -29,6 +30,45 @@ class FollowingTest < Following
     @user.followers.each do |user|
       # There should be 3 links: sidebar gravatar, user name and gravatar
       assert_select 'a[href=?]', user_path(user), count: 3
+    end
+  end
+end
+
+class FollowTest < Following
+  test 'should follow a user the standard way' do
+    assert_difference '@user.following.count', 1 do
+      post relationships_path, params: { followed_id: @other.id }
+    end
+    assert_redirected_to @other
+  end
+
+  test 'should follow a user with Hotwire' do
+    assert_difference '@user.following.count', 1 do
+      post relationships_path(format: :turbo_stream),
+           params: { followed_id: @other.id }
+    end
+  end
+end
+
+class Unfollow < Following
+  def setup
+    super
+    @user.follow(@other)
+    @relationship = @user.active_relationships.find_by(followed_id: @other.id)
+  end
+end
+
+class UnfollowTest < Unfollow
+  test 'should unfollow a user the standard way' do
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(@relationship)
+    end
+    assert_redirected_to @other
+  end
+
+  test 'should unfollow a user with Hotwire' do
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(@relationship, format: :turbo_stream)
     end
   end
 end
