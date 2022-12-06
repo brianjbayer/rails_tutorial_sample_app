c328876fef92afe26069ea26934945a8e5309107
brianjbayer <brianjbayer@gmail.com>
2022-12-06 13:46:21 -0500

    Add integration test for user's home page feed

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -72,3 +72,12 @@ class UnfollowTest < Unfollow
     end
   end
 end
+
+class FollowingPagesTest < Following
+  test 'feed on Home page' do
+    get root_path
+    @user.feed.paginate(page: 1).each do |micropost|
+      assert_match CGI.escapeHTML(micropost.content), response.body
+    end
+  end
+end
